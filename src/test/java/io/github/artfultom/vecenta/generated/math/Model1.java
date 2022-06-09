9bd599e0504941c27a27a6e950ff8f4c98cb437b
Andrey Vasilyev <artfultom@protonmail.com>
2022-06-09 13:57:37 +0300

    [Add] missed file

---
+++ src/test/java/io/github/artfultom/vecenta/generated/math/Model1.java
@@ -0,0 +1,16 @@
+package io.github.artfultom.vecenta.generated;
+
+public class Model1 {
+    public Integer field1;
+
+    public Model1() {
+    }
+
+    public Integer getField1() {
+        return this.field1;
+    }
+
+    public void setField1(Integer field1) {
+        this.field1 = field1;
+    }
+}
